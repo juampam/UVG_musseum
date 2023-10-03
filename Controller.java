049b8005259152253b030ebde0d49beffe858743
juampam <jpablomuralles@gmail.com>
2023-10-02 21:21:19 -0500

    controller

---
+++ Controller.java
@@ -0,0 +1,23 @@
+package Musseum;
+
+public class Controller{
+
+	Dino dinosour = new Dino()
+	Plant plant = new Plant()
+	Mammal prehmamm = new Mammal()
+	
+	public void createSpecimen(){
+		int selector;
+		System.out.Println("New Specimen\nPlease select a type");
+		System.out.Println("\n1. Dinosour\n2. Plant\n3. Mammal");
+
+		switch(selector){
+			case 1:
+			break;
+			case 2:
+			break;
+			case 3:
+			break;
+		}
+	}
+}
